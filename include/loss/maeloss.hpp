ad900b0de993343ca3a4147f1b24d26f460709f2
Alexey <myk1n.alex@gmail.com>
2026-05-12 17:33:21 +0300

    MAELoss

---
+++ include/loss/maeloss.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "lossfunction.hpp"
+#include <numeric>
+#include <algorithm>
+
+class MAELoss : public LossFunction {
+public:
+    MAELoss(double lr) : LossFunction(lr, 1) {}
+
+    static double compute_median(const std::vector<double>& v) {
+        if (v.empty()) return 0.0;
+        std::vector<double> sorted = v;
+        std::sort(sorted.begin(), sorted.end());
+        return sorted[sorted.size() / 2];
+    }
+
+    std::vector<double> initial_prediction(const std::vector<double> &y) const override {
+        double median = compute_median(y);
+        return std::vector<double>(y.size(), median);
+    }
+
+    std::vector<double> gradient(const std::vector<double>& y, const std::vector<double>& y_pred) const override {
+        std::vector<double> grad(y.size());
+        for (size_t i = 0; i < y.size(); ++i) {
+            grad[i] = (y[i] >= y_pred[i]) ? 1.0 : -1.0;
+        }
+        return grad;
+    }
+
+    std::vector<double> transform(const std::vector<double>& raw) const override {
+        return raw;
+    }
+};
